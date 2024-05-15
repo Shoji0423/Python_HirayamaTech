8f3553663cb1108dd755ced76bb0f7c82df8fd36
HIRAYAMAISGOD <hirayama.hiroto.mf@mynavi.jp>
2024-05-15 05:02:12 +0000

    アニマルを追加

--- hirayama/animallist.py
+++ hirayama/animallist.py
@@ -3,7 +3,10 @@ args=sys.argv
 number=int(args[1])
 add_animal=args[2]
 animal=["giraffe", "tiger", "zebra", "elephant", "lion"]
+#指定した位置に動物名を追加
 animal.insert(number,add_animal)
+#リストの末尾の要素を削除
 animal.pop()
+#ソート
 animal.sort()
 print(animal)
