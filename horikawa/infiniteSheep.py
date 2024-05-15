b4da35d2566f1ec67a5bcd51bbe577889618b376
Shoji0423 <Horikawa.shoji.hg@mynavi.jp>
2024-05-15 04:33:53 +0000

    infinitySheep.pyにコメントを追加

--- horikawa/infiniteSheep.py
+++ horikawa/infiniteSheep.py
@@ -1,11 +1,15 @@
-#
+# 入力された数字に到達するまで羊を数える
+
+# whileループで実施
 import sys
 
+# 最大数
 max_count = int(sys.argv[1])
 
+# ループ実行回数
 count = 0
 
+# 0スタートで1匹ずつ増えるループ
 while count < max_count:
     count += 1
-
     print("ひつじが{0}匹".format(count))
