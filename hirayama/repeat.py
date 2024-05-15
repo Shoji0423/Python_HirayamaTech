d541b8eb7cba8f7e7586a64672eb29133f15659f
HIRAYAMAISGOD <hirayama.hiroto.mf@mynavi.jp>
2024-05-15 02:06:55 +0000

    永遠の足し算プログラムを追加

---
+++ hirayama/repeat.py
@@ -0,0 +1,11 @@
+import sys
+args=sys.argv
+number=int(args[1])
+sum=0
+while sum <= 100 :
+    sum+=number
+    if sum == 100:
+        print("Just 100!" ,end="")
+        break
+else:
+    print("Over!",end="")
