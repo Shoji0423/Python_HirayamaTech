2bd7ab39802bafac09f6578f8bd5a5f54dfb81a9
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-15 05:43:45 +0000

    Revert "ADD population"
    This reverts commit df5590e89d2cdbe9c09011d034e9da67864da4b1.

--- shinhon/population.py
+++
@@ -1,6 +0,0 @@
-import sys
-num = int(sys.argv[1])
-
-countries = ("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")
-
-print(countries[num-1])
