59a26c6eb44ccef66243fbc51d70db315b1c48e9
Shoji0423 <Horikawa.shoji.hg@mynavi.jp>
2024-05-15 02:08:17 +0000

    足し算続ける処理 endlessAdd.pyを作成

---
+++ horikawa/endlessAdd.py
@@ -0,0 +1,20 @@
+# 入力された数値を足すループ処理
+# ちょうど100なら「Just 100!」、超えたら「Over!」と表示して終了
+
+import sys
+# 基盤の数値、合計
+baseNum = int(sys.argv[1])
+sum = 0
+
+while sum < 100:
+    sum += baseNum
+
+    # 判定　100ぴったり
+    if sum == 100:
+        print("Just 100!", end="")
+        break
+    # 判定　100オーバー
+    elif sum > 100:
+        print("Over!", end="")
+        break
+
