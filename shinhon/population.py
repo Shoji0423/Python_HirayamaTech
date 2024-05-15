64ce2db310642633fdf22fd81f07bffb49468c05
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-15 05:45:53 +0000

    ADD population

---
+++ shinhon/population.py
@@ -0,0 +1,6 @@
+import sys
+num = int(sys.argv[1])
+
+countries = ("China", "India", "U.S.A", "Indonesia", "Pakistan", "Brazil", "Nigeria", "Bangladesh", "Russia", "Mexico")
+
+print(countries[num-1])
