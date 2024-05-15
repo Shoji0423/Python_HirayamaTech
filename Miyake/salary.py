ab194281715ac429143d2355d1c32b2c8fafc7dc
MokaMiyake <miyake.moka.dr@mynavi.jp>
2024-05-15 01:29:29 +0000

    給与の支給額の計算

---
+++ Miyake/salary.py
@@ -0,0 +1,17 @@
+import sys
+from decimal import Decimal, ROUND_HALF_UP
+args = sys.argv
+
+#引数を変数に代入
+salary = int(args[1])
+#税額の計算
+if (salary > 1000000):
+    tax = (salary-1000000) * 0.2 + 100000
+elif(salary <= 1000000):
+    tax = salary * 0.1
+#四捨五入の計算
+tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+#支給額の計算
+pay_amount = salary - tax
+#表示
+print("支給額:{0}、税額:{1}".format(pay_amount, tax),end="")
