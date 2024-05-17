d66f4537c1608cb91ada0cb80bec09b5a2e4b0c4
HIRAYAMAISGOD <hirayama.hiroto.mf@mynavi.jp>
2024-05-17 01:50:30 +0000

    Merge remote-tracking branch 'refs/remotes/origin/main'

---
+++ hirayama/primefactor.py
@@ -0,0 +1,19 @@
+import sys
+args=sys.argv
+number=int(args[1])
+number_list=[]
+
+while number!=1:
+    i=3
+    if number % 2==0:
+        number=number/2
+        number_list.append(2)
+    else:
+        while True:
+            if number % i ==0:
+                number=number/i
+                number_list.append(i)
+                break
+            i+=2
+print(number_list)
+        
