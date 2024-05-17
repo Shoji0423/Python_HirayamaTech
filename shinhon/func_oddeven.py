a58b8b0a0b14970da6263f3323280178b49dfb25
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-17 04:45:41 +0000

    Change list to INT type

--- shinhon/func_oddeven.py
+++ shinhon/func_oddeven.py
@@ -1,12 +1,12 @@
 # inputに引数を代入したあと、数値をnum_listにまとめる
 import sys
 input = sys.argv
-num_list = input[1:]
+num_list = list(map(int, input[1:]))
 
 # 数値を判断するための関数
 def oddeven_checker(num):
     '''引数は奇数と偶数かを判断する'''
-    if int(num) % 2:
+    if num % 2:
         print(f'{num}は奇数')
     else:
         print(f'{num}は偶数')
