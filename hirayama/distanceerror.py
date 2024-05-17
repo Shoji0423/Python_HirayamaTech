50271a58b50702610f4a87e17d34f9af56e7f48e
HIRAYAMAISGOD <hirayama.hiroto.mf@mynavi.jp>
2024-05-17 05:21:07 +0000

    aa

---
+++ hirayama/distanceerror.py
@@ -0,0 +1,11 @@
+import sys
+args=sys.argv
+basyo1=args[1]
+basyo2=args[2]
+#駅名と距離の辞書を定義
+station={"東京":0.00,"品川":6.78,"新横浜":25.54,"名古屋":342.02,"京都":476.31,"新大阪":515.35}
+try:
+    #東京駅からの距離を出力
+    print(round(abs(station[basyo1]-station[basyo2]),2))
+except:
+    print("のぞみの停車駅を引数に設定してください")
