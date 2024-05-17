9c8e813fbd0c2d11a44480a3a9e090585b4f821b
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-17 04:39:36 +0000

    ADD func_oddeven

---
+++ shinhon/func_oddeven.py
@@ -0,0 +1,16 @@
+# inputに引数を代入したあと、数値をnum_listにまとめる
+import sys
+input = sys.argv
+num_list = input[1:]
+
+# 数値を判断するための関数
+def oddeven_checker(num):
+    '''引数は奇数と偶数かを判断する'''
+    if int(num) % 2:
+        print(f'{num}は奇数')
+    else:
+        print(f'{num}は偶数')
+
+# リストの中にある数値の判断結果を出力
+for i in num_list:
+    oddeven_checker(i)
