622f454bae6c8cdb663c8f4da27294b844d9efab
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-20 01:45:06 +0000

    ADD comment and filename

--- shinhon/makeqrcode.py
+++ shinhon/makeqrcode.py
@@ -1,12 +1,11 @@
-import qrcode
+import qrcode #Qrcodeを作成するためのツール
 import sys
-import os
+import os #ローカルのパスを把握するためのツール
 
+# リンクとファイル名を定義
 link = str(sys.argv[1])
+filename = str(sys.argv[2])
 
 img = qrcode.make(link)
 
-print(os.path)
-print(os.path.join("../..", "files", "qrcode.png"))
-
-img.save(os.path.join("../..", "files", "qrcode.png"))
+img.save(os.path.join("../..", "files", filename))
