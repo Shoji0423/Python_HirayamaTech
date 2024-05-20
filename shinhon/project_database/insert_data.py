70a99c651f2deb2e2a924e5ecc43b931c1dbb04a
lai-shinhon <lai.xinhong.au@mynavi.jp>
2024-05-20 07:50:39 +0000

    Update database

--- shinhon/project_database/insert_data.py
+++ shinhon/project_database/insert_data.py
@@ -1,2 +1,11 @@
 from datetime import date
 from database import session
+from tables import Holiday
+
+holiday = Holiday(
+    holi_date = date(2024, 4, 1),
+    holi_text = "シャチクの日"
+)
+
+session.add(holiday)
+session.commit()
--- shinhon/project_database/select_data.py
+++ shinhon/project_database/select_data.py
@@ -2,9 +2,10 @@ from database import session
 from tables import Holiday
 from datetime import date
 
-# holidaylist = session.query(Holiday).all()
-holidaylist = session.query(Holiday).filter(Holiday.holi_date < date(2022, 11, 1))
+holidaylist = session.query(Holiday).all()
+# holidaylist = session.query(Holiday).filter(Holiday.holi_date < date(2022, 11, 1))
 
 for item in holidaylist:
+    data = item
     print(item.holi_date, end=" ")
     print(item.holi_text)
